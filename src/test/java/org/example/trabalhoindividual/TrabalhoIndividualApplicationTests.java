5969584dd14040b036c637c4ef2ad6a4a8fdd0a4
Guiferote <guilherme.guingo07@gmail.com>
2026-05-12 22:13:30 -0300

    feature/setup/setup-inicial-finalizado

---
+++ src/test/java/org/example/trabalhoindividual/TrabalhoIndividualApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.trabalhoindividual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoIndividualApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
